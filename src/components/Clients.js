e8630d7729c7936c5725e6bfc5cea08866ba08fa
fortranius <fortranius@bk.ru>
2018-12-06 18:56:59 +0300

    create update clients

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -6,24 +6,109 @@ import PageParams from "../model/PageParams";
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 import {getClientsAction} from "../actions/clientActions";
-import {removeClient} from "../service/clientService";
+import {removeClient, updateClient, createClient} from "../service/clientService";
+import DeleteModal from "../modal/DeleteModal";
+import UpdateModal from "../modal/UpdateModal";
 
 class Clients extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            openDelete: false,
+            openUpdate: false,
+            openCreate: false,
+            row: undefined
+        };
         this.handleTableChange = this.handleTableChange.bind(this);
+
         this.removeClient = this.removeClient.bind(this);
+        this.onOpenDeleteModal = this.onOpenDeleteModal.bind(this);
+        this.onCloseDeleteModal = this.onCloseDeleteModal.bind(this);
+
+        this.updateClient = this.updateClient.bind(this);
+        this.onOpenUpdateModal = this.onOpenUpdateModal.bind(this);
+        this.onCloseUpdateModal = this.onCloseUpdateModal.bind(this);
+
+        this.createClient = this.createClient.bind(this);
+        this.onOpenCreateModal = this.onOpenCreateModal.bind(this);
+        this.onCloseCreateModal = this.onCloseCreateModal.bind(this);
+
         this.props.clientActions(new PageParams(0, 10));
     }
+
+    onOpenDeleteModal (row) {
+        this.setState({
+            openDelete: true,
+            row: row
+        });
+    };
+
+    onOpenUpdateModal (row) {
+        this.setState({
+            openUpdate: true,
+            row: row
+        });
+    };
+
+    onOpenCreateModal () {
+        this.setState({
+            openCreate: true,
+        });
+    };
+
+    onCloseDeleteModal = () => {
+        this.setState({
+            openDelete: false,
+            row: undefined
+        });
+    };
+
+    onCloseUpdateModal = () => {
+        this.setState({
+            openUpdate: false,
+            row: undefined
+        });
+    };
+
+    onCloseCreateModal = () => {
+        this.setState({
+            openCreate: false
+        });
+    };
 
     handleTableChange = (type, {page, sizePerPage}) => {
         this.props.clientActions(new PageParams(page - 1, sizePerPage));
     };
 
+    updateClient(entity) {
+        let client = this.state.row;
+        client.person = entity.person;
+        updateClient(client).then(() => {
+            this.props.clientActions(new PageParams(this.props.clients.number, this.props.clients.size));
+            this.setState({
+                openUpdate: false,
+                row: undefined
+            });
+        });
+    };
+
+    createClient(entity) {
+        createClient(entity).then(() => {
+            this.props.clientActions(new PageParams(this.props.clients.number, this.props.clients.size));
+            this.setState({
+                openCreate: false
+            });
+        });
+    };
+
     removeClient(id) {
         removeClient(id).then(() => {
             this.props.clientActions(new PageParams(this.props.clients.number, this.props.clients.size));
+            this.setState({
+                openDelete: false,
+                row: undefined
+            });
         });
     };
 
@@ -33,11 +118,29 @@ class Clients extends Component {
                 {this.props.clients ? <TableRemote data={this.props.clients.content}
                                                    page={this.props.clients.number + 1}
                                                    columns={colClient}
-                                                   remove={this.removeClient}
+                                                   entity="клиента"
+                                                   remove={this.onOpenDeleteModal}
+                                                   update={this.onOpenUpdateModal}
+                                                   create={this.onOpenCreateModal}
                                                    sizePerPage={this.props.clients.size}
                                                    totalSize={this.props.clients.totalElements}
                                                    onTableChange={this.handleTableChange}/>
                     : null}
+                <DeleteModal accept={this.removeClient}
+                             open={this.state.openDelete}
+                             close={this.onCloseDeleteModal}
+                             entity="клиента" />
+
+                {this.state.row ? <UpdateModal accept={this.updateClient}
+                                               open={this.state.openUpdate}
+                                               update={this.state.row}
+                                               close={this.onCloseUpdateModal}
+                                               entity="клиента" />: null}
+
+                <UpdateModal accept={this.createClient}
+                             open={this.state.openCreate}
+                             close={this.onCloseCreateModal}
+                             entity="клиента" />
             </div>
         );
     }
--- src/service/clientService.js
+++ src/service/clientService.js
@@ -13,6 +13,27 @@ export function removeClient(clientId) {
         .then(function(res){ return res })
 }
 
+export function updateClient(master) {
+    return sendRequest(master, "PUT");
+}
+
+export function createClient(master) {
+    return sendRequest(master, "POST");
+}
+
+function sendRequest(master, method) {
+
+    const options = {
+        method: method,
+        headers: new Headers({'content-type': 'application/json'}),
+        body: JSON.stringify(master)
+    };
+
+    return fetch("http://localhost:8080/api/client", options)
+        .then(handleErrors)
+        .then(function(res){ return res })
+}
+
 function handleErrors(response) {
     if (!response.ok) {
         throw Error(response.statusText);
