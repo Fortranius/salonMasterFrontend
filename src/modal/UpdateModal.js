809d8392d7fdbf45c763a9f35ff04577babde629
fortranius <fortranius@bk.ru>
2018-12-06 19:49:33 +0300

    validation

--- src/modal/UpdateModal.js
+++ src/modal/UpdateModal.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import '../App.css';
 import Modal from 'react-responsive-modal';
-import TextField from '@material-ui/core/TextField';
-import { withStyles } from '@material-ui/core/styles';
-import MaskedInput from 'react-text-mask';
-import InputLabel from '@material-ui/core/InputLabel';
+import {withStyles} from '@material-ui/core/styles';
+import NumberFormat from 'react-number-format';
 import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
 import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
 
 const styles = theme => ({
     container: {
@@ -27,16 +27,23 @@ const styles = theme => ({
     },
 });
 
-function PhoneCustom(props) {
-    const { inputRef, ...other } = props;
+function NumberFormatCustom(props) {
+    const { inputRef, onChange, ...other } = props;
 
     return (
-        <MaskedInput
+        <NumberFormat
             {...other}
-            ref={inputRef}
-            mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
-            placeholderChar={'\u2000'}
-            showMask
+            getInputRef={inputRef}
+            format="+7 (###) ###-####" mask="_"
+            onValueChange={values => {
+                onChange({
+                    target: {
+                        value: values.value,
+                    },
+                });
+            }}
+            thousandSeparator
+            prefix="$"
         />
     );
 }
@@ -52,7 +59,8 @@ class UpdateModal extends Component {
                 surname:'',
                 patronymic:'',
                 mail:'',
-            }
+            },
+            submit: false
         };
         this.refused = this.refused.bind(this);
         this.accept = this.accept.bind(this);
@@ -77,6 +85,14 @@ class UpdateModal extends Component {
     };
 
     accept = () => {
+        this.setState({
+            submit: true
+        });
+        if (this.state.person.name
+            && this.state.person.surname
+            && this.state.person.patronymic
+            && this.state.person.mail
+            && this.state.person.phone.length === 10)
         this.props.accept(this.state);
     };
 
@@ -89,6 +105,14 @@ class UpdateModal extends Component {
         });
     };
 
+    validate(field) {
+        if (!this.state.submit)
+            return false;
+        if (field === 'phone')
+            return this.state.person.phone.length !== 10;
+        return (!this.state.person || !this.state.person[field]);
+    }
+
     render() {
         const { classes } = this.props;
         return (
@@ -100,48 +124,32 @@ class UpdateModal extends Component {
                        closeOnEsc={false} center={false}>
                     { this.props.update ? <h2>Редактирование {this.props.entity}</h2>: null }
                     { !this.props.update ? <h2>Создание {this.props.entity}</h2>: null }
-                    <div className="form-group">
-                        <TextField
-                            label="Фамилия"
-                            value={this.state.person.surname}
-                            onChange={this.handleChange('surname')}
-                            className={classes.textField}
-                            type="text"
-                            margin="normal"
-                        />
-                        <TextField
-                            label="Имя"
-                            value={this.state.person.name}
-                            onChange={this.handleChange('name')}
-                            className={classes.textField}
-                            type="text"
-                            margin="normal"
-                        />
-                        <TextField
-                            label="Отчество"
-                            value={this.state.person.patronymic}
-                            onChange={this.handleChange('patronymic')}
-                            className={classes.textField}
-                            type="text"
-                            margin="normal"
-                        />
-                        <FormControl className={classes.formControl}>
-                            <InputLabel htmlFor="phone">Телефон</InputLabel>
-                            <Input
-                                value={this.state.person.phone}
-                                id="phone"
-                                onChange={this.handleChange('phone')}
-                                inputComponent={PhoneCustom}
-                            />
+                    <div className={classes.container}>
+                        <FormControl className={classes.formControl} error={this.validate('surname')} aria-describedby="surname-error-text">
+                            <InputLabel htmlFor="surname">Фамиля</InputLabel>
+                            <Input id="surname" value={this.state.person.surname} onChange={this.handleChange('surname')} />
+                            { this.validate('surname') ? <FormHelperText id="surname-error-text">Поле не может быть пустым</FormHelperText>: null }
                         </FormControl>
-                        <TextField
-                            label="Почта"
-                            value={this.state.person.mail}
-                            onChange={this.handleChange('mail')}
-                            className={classes.textField}
-                            type="text"
-                            margin="normal"
-                        />
+                        <FormControl className={classes.formControl} error={this.validate('name')} aria-describedby="name-error-text">
+                            <InputLabel htmlFor="name">Имя</InputLabel>
+                            <Input id="name" value={this.state.person.name} onChange={this.handleChange('name')} />
+                            { this.validate('name') ? <FormHelperText id="name-error-text">Поле не может быть пустым</FormHelperText>: null }
+                        </FormControl>
+                        <FormControl className={classes.formControl} error={this.validate('patronymic')} aria-describedby="patronymic-error-text">
+                            <InputLabel htmlFor="patronymic">Отчество</InputLabel>
+                            <Input id="patronymic" value={this.state.person.patronymic} onChange={this.handleChange('patronymic')} />
+                            { this.validate('patronymic') ? <FormHelperText id="patronymic-error-text">Поле не может быть пустым</FormHelperText>: null }
+                        </FormControl>
+                        <FormControl className={classes.formControl} error={this.validate('phone')} aria-describedby="phone-error-text">
+                            <InputLabel htmlFor="phone">Телефон</InputLabel>
+                            <Input id="phone" value={this.state.person.phone} inputComponent={NumberFormatCustom} onChange={this.handleChange('phone')} />
+                            { this.validate('phone') ? <FormHelperText id="phone-error-text">Телефон введен неверно</FormHelperText>: null }
+                        </FormControl>
+                        <FormControl className={classes.formControl} error={this.validate('mail')} aria-describedby="mail-error-text">
+                            <InputLabel htmlFor="mail">Почта</InputLabel>
+                            <Input id="mail" value={this.state.person.mail} onChange={this.handleChange('mail')} />
+                            { this.validate('mail') ? <FormHelperText id="mail-error-text">Поле не может быть пустым</FormHelperText>: null }
+                        </FormControl>
                     </div>
                     <div className="button-group">
                         <button className="btn btn-primary" onClick={this.accept}>
