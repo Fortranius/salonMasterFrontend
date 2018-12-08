9520f00c9ae71132f4106c85168221e7f66d7892
fortranius <fortranius@bk.ru>
2018-12-08 19:15:13 +0300

    timeslot data

--- src/modal/TimeSlotModal.js
+++ src/modal/TimeSlotModal.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import '../App.css';
 import Modal from 'react-responsive-modal';
 import {withStyles} from '@material-ui/core/styles';
+import AsyncSelect from 'react-select/lib/Async';
 
 const styles = theme => ({
     container: {
@@ -22,10 +23,41 @@ const styles = theme => ({
     },
 });
 
+export const colourOptions = [
+    { value: 'ocean', label: 'Ocean', color: '#00B8D9', isFixed: true },
+    { value: 'blue', label: 'Blue', color: '#0052CC', disabled: true },
+    { value: 'purple', label: 'Purple', color: '#5243AA' },
+    { value: 'red', label: 'Red', color: '#FF5630', isFixed: true },
+    { value: 'orange', label: 'Orange', color: '#FF8B00' },
+    { value: 'yellow', label: 'Yellow', color: '#FFC400' },
+    { value: 'green', label: 'Green', color: '#36B37E' },
+    { value: 'forest', label: 'Forest', color: '#00875A' },
+    { value: 'slate', label: 'Slate', color: '#253858' },
+    { value: 'silver', label: 'Silver', color: '#666666' },
+];
+
+const filterColors = (inputValue) => {
+    if (inputValue) {
+        return colourOptions.filter(i =>
+            i.label.toLowerCase().includes(inputValue.toLowerCase())
+        );
+    }
+    return colourOptions;
+};
+
+const loadOptions = (inputValue, callback) => {
+    setTimeout(() => {
+        callback(filterColors(inputValue));
+    }, 1000);
+};
+
 class TimeSlotModal extends Component {
 
     constructor() {
         super();
+        this.state = {
+            inputValue: ''
+        };
         this.refused = this.refused.bind(this);
         this.accept = this.accept.bind(this);
     }
@@ -36,6 +68,12 @@ class TimeSlotModal extends Component {
 
     accept = () => {
         this.props.accept(this.state);
+    };
+
+    handleInputChange = (newValue) => {
+        const inputValue = newValue.replace(/\W/g, '');
+        this.setState({ inputValue });
+        return inputValue;
     };
 
     render() {
@@ -75,6 +113,14 @@ class TimeSlotModal extends Component {
                             </div>
                             <hr/>
                         </div>
+                        <hr/>
+                        <AsyncSelect
+                            cacheOptions
+                            loadOptions={loadOptions}
+                            defaultOptions
+                            onInputChange={this.handleInputChange}
+                        />
+                        <hr/>
                         </div>: null }
                     <div className="button-group">
                         <button className="btn btn-primary" onClick={this.accept}>
